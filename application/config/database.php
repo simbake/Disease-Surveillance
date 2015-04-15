3829b7b6ef06810679b2eee0b5c00df0e53101be
Mecer2143 <simbake2009@yahoo.com>
2015-04-15 16:32:48 +0300

    changes to config/database.php. added gitignore

--- application/config/database.php
+++ application/config/database.php
@@ -43,7 +43,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'Mecer2143';
 $db['default']['database'] = 'idsr_demo';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
